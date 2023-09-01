9335a75e82154f829c7ce278f44814febf5d5185
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-09-01 19:32:56 +0200

    add: last message in list contacts

--- js/main.js
+++ js/main.js
@@ -12,9 +12,21 @@ createApp({
     selectChat(index) {
       this.activeChat = index;
     },
+
+    lastMessage(contact, index) {
+      let lastMessage = contact.messages[contact.messages.length - 1];
+      console.log(lastMessage);
+      let lastMessageText = lastMessage.message;
+      console.log(lastMessageText);
+      return lastMessageText;
+    },
   },
 
   created() {
-    console.log(this.contacts);
+    // console.log(this.contacts);
+  },
+
+  conputed() {
+    this.lastMessage();
   },
 }).mount("#app");
